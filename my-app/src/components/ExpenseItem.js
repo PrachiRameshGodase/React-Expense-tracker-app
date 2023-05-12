6f9bdb2c6733aa35c74d241b275678773e66d5eb
PrachiRameshGodase <prachigodase2@gmail.com>
2023-05-12 09:05:18 +0530

    Adding date readable

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -2,13 +2,16 @@ import React from 'react'
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  // const expenseDate=new Date(2021,2,28);
-  // const expenseTitle="Car Insurance";
-  // const expenseAmount=294.67; 
-  // const LocationOfExpenditure="office";
+  const month=props.date.toLocaleString("en-Us",{month:"long"});
+  const day=props.date.toLocaleString("en-Us",{day:"2-digit"});
+  const year=props.date.getFullYear();
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <h2>{props.location}</h2>
