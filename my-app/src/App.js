65a2fbe4981e1a97a0fa5e8aedf1cebbfbdb6099
PrachiRameshGodase <prachigodase2@gmail.com>
2023-05-14 15:01:32 +0530

    Creating own first Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import './App.css';
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App=()=> {
@@ -37,7 +38,7 @@ const App=()=> {
   // React.createElement("h2",{},"Let's get started!"),React.createElement(Expenses,{items:expenses}))
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses}/>
     </div>
   );
